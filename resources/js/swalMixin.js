a35acafd03a5cfef5c4001ef229d91bf1e1910e0
Mukramin <mukramin0909@gmail.com>
2023-01-21 01:17:18 +0800

    Global delete confirmation with Swal

--- resources/js/swalMixin.js
+++ resources/js/swalMixin.js
@@ -12,6 +12,21 @@ export const SwalMixin = {
         timer: 3000,
         timerProgressBar: true
       })
+    },
+    swalConfirm(callback) {
+      return this.$swal({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          callback();
+        }
+      })
     }
   }
 }
